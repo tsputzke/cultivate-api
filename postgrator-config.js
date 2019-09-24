5ebc1d20c909e88c41e1954130b37ef6ad69a1a1
Timothy Putzke <tsputzke@gmail.com>
2019-09-24 10:39:41 -0400

    Prep for deploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,16 +1,5 @@
 require('dotenv').config();
 
-// module.exports = {
-//   "migrationDirectory": "migrations",
-//   "driver": "pg",
-//   "host": process.env.PROD_MIGRATION_DB_HOST,
-//   "port": process.env.PROD_MIGRATION_DB_PORT,
-//   "database": process.env.PROD_MIGRATION_DB_NAME,
-//   "username": process.env.PROD_MIGRATION_DB_USER,
-//   "password": process.env.PROD_MIGRATION_DB_PASS,
-//   "ssl": true
-// }
-
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
@@ -19,5 +8,8 @@ module.exports = {
   "database": process.env.MIGRATION_DB_NAME,
   "username": process.env.MIGRATION_DB_USER,
   "password": process.env.MIGRATION_DB_PASS,
-  "ssl": false
+  "ssl": false,
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://tsputzke@localhost/cultivate',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://tsputzke@localhost/cultivate',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://tsputzke@localhost/cultivate',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
 }
