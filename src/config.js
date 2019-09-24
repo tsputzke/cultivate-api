a95dda317e9bdb2b6dbcf600c4ef29cf1b412599
Timothy Putzke <tsputzke@gmail.com>
2019-09-24 11:59:57 -0400

    Prep for deploy

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://tsputzke@localhost/cultivate',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://tsputzke@localhost/cultivate-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+  "ssl": !!process.env.SSL,
 }
