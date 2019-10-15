5cb307375249878fca3836baeee58f47520e0cb8
Timothy Putzke <tsputzke@gmail.com>
2019-10-15 15:02:21 -0400

    Increase expiry to 5 hours

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://tsputzke@localhost/cultivate',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://tsputzke@localhost/cultivate-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '5h',
   "ssl": true,
 }
